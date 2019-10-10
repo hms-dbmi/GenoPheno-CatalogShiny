24cf864e24cb2ce8deed4b3e2f4f07b555dbb7c8
Carlos De Niz <carlitre@gmail.com>
2019-10-10 11:09:55 -0400

    Update html fields

--- dbGapSelection.R
+++ dbGapSelection.R
@@ -1,21 +1,30 @@
 #####################
 ## Data extraction ##
 #####################
-#https://www.ncbi.nlm.nih.gov/projects/gapsolr/facets.html
-#Click on Save results as CSV
+
+rm(list = ls())
+# Installs and loads packages
+paket <- function(pak){
+  new_pak <- pak[!(pak %in% rownames(installed.packages()))]
+  if (length(new_pak))
+    install.packages(new_pak, dependencies = TRUE,repos='http://cran.us.r-project.org')
+  sapply(pak, library, character.only = TRUE)
+}
+listOfPackages = c("data.table","stringr")
+paket(listOfPackages)
 
 ###################
 ## dbGap studies ##
 ###################
 
+#https://www.ncbi.nlm.nih.gov/projects/gapsolr/facets.html
+#Click on Save results as CSV
+
 file_path = "/Users/carlos/Desktop/Harvard_18_19/dbGAP/"
 
-
-library(stringr)
 #read the csv file
 dbgap <- read.delim(paste0(file_path,"dbGAP_All.csv"), sep = ",")
 colnames(dbgap)
-
 
 #select the columns needed
 dbgapSelection <- dbgap[, c("name", "Study.Design", "Study.Disease.Focus","Study.Molecular.Data.Type", 
@@ -27,17 +36,13 @@ dbgapSelection$link <- paste0( "https://www.ncbi.nlm.nih.gov/projects/gap/cgi-bi
 
 
 ## extract the number of samples and subjects for each study
-dbgapStudyContent <- dbgap[, c("accession", "Study.Content")]
+dbgapStudyContent <- data.table(dbgap[, c("accession", "Study.Content")])
 dbgapStudyContent <- dbgapStudyContent[ dbgapStudyContent$accession %in% dbgapSelection$accession, ]
-dbgapStudyContent$Study.Content <- as.character( dbgapStudyContent$Study.Content)
+dbgapStudyContent <- dbgapStudyContent[,Study.Content:=as.character(Study.Content)]
+# Add columns 
+add_cols <- c("phenotype.dataset","variables","documents","analyses","molecular.datasets","subjects","samples")
+dbgapStudyContent = dbgapStudyContent[,c(add_cols):=list(NA)]
 
-dbgapStudyContent$phenotype.dataset <- NA
-dbgapStudyContent$variables <- NA
-dbgapStudyContent$documents <- NA
-dbgapStudyContent$analyses <- NA
-dbgapStudyContent$molecular.datasets <- NA
-dbgapStudyContent$subjects <- NA
-dbgapStudyContent$samples <- NA
 
 for(i in 1:nrow( dbgapStudyContent ) ){
   print(i)
@@ -65,32 +70,14 @@ for(i in 1:nrow( dbgapStudyContent ) ){
   }
 }
 
-
-dbgapStudyContent <- dbgapStudyContent[, c(1, 3:9)]
 dbgapStudyContent <- dbgapStudyContent[, c("accession", "variables", "subjects", "samples")]
 finalSet <- merge( dbgapSelection, dbgapStudyContent, by="accession")
 
-##remove those with less than 30 variables
-#finalSetFilter <- finalSet[!is.na( finalSet$variables), ]
-#nrow(finalSetFilter) #1135
-
-#finalSetFilter <- finalSetFilter[ as.numeric(finalSetFilter$variables) >= 100, ]
-#nrow(finalSetFilter) #154
-
-#finalSetFilter <- finalSetFilter[ as.numeric(finalSetFilter$samples) >= 100, ]
-#nrow(finalSetFilter) #280
-
-#finalSetFilter <- finalSetFilter[ as.numeric(finalSetFilter$subjects) >= 1000, ]
-#nrow(finalSetFilter) #160
-
 ##add the columns that we need and sort the table
-finalSetFilter <- finalSet
-finalSetFilter$Country <- NA
-finalSetFilter$Patients.Age <- NA
-finalSetFilter$Notes <- NA
-finalSetFilter$PubMedLink <- NA
-finalSetFilter$Phenotypic.Data.Type <- NA
-
+finalSetFilter <- data.table(finalSet)
+# Add columns 
+add_cols <- c("Country","Patients.Age","Notes","PubMedLink","Phenotypic.Data.Type")
+finalSetFilter = finalSetFilter[,c(add_cols):=list(NA)]
 
 finalSetFilterSort <- finalSetFilter[, c("name", "Country", "samples", "subjects", "Study.Design",
                                      "variables", "Phenotypic.Data.Type","Study.Molecular.Data.Type", "Study.Markerset", 
@@ -98,41 +85,6 @@ finalSetFilterSort <- finalSetFilter[, c("name", "Country", "samples", "subjects
                                      "Study.Consent", "accession", "link", "PubMedLink", "Notes")]
 
 finalSetFilterSort$dbGaP.TOPMed.Study.Accession <-  sapply(strsplit( as.character(finalSetFilterSort$accession), "[.]"), '[', 1)
-
-##check which ones are in TopMED freeze 8
-#we download the information from https://www.nhlbiwgs.org/topmed-whole-genome-sequencing-methods-freeze-8
-# topmed <- read.delim("/Users/alba/Desktop/biobankPaper/Biobanks/catalogBiobanksTopMED.csv", sep=',')
-# 
-# for( i in 1:nrow(finalSetFilterSort)){
-#   if( finalSetFilterSort$dbGaP.TOPMed.Study.Accession[i] %in% topmed$dbGaP.TOPMed.Study.Accession ){
-#     finalSetFilterSort$Notes[i] <- "TOPMed Freeze 8"
-#   }
-#   if( finalSetFilterSort$dbGaP.TOPMed.Study.Accession[i] %in% topmed$dbGaP.Parent.Study.Accession ){
-#     finalSetFilterSort$Notes[i] <- "TOPMed Freeze 8"
-#   }
-# }
-# 
-# finalSetFilterSort <- finalSetFilterSort[, c(1:17)]
-# colnames(finalSetFilterSort) <- c("Name", "Country", "Sample Size", "Subject Count", "Study Design",
-#                                   "Phenotypic Variables", "Phenotypic Data Type", "Molecular Data Type", "Markerset", 
-#                                   "Disease/Focus", "Patients Age", "Ancestry", 
-#                                   "Consent", "Accession", "Link", "PubMedLink", "Notes")
-# 
-# 
-# finalSetFilterSort$Name <- gsub( ",", ";", finalSetFilterSort$Name)
-# finalSetFilterSort$`Molecular Data Type` <- gsub( ",", ";", finalSetFilterSort$`Molecular Data Type`)
-# finalSetFilterSort$`Disease/Focus` <- gsub( ",", ";", finalSetFilterSort$`Disease/Focus`)
-# finalSetFilterSort$Markerset <- gsub( ",", ";", finalSetFilterSort$Markerset)
-# finalSetFilterSort$Ancestry <- gsub( ",", ";", finalSetFilterSort$Ancestry)
-# finalSetFilterSort$Consent <- gsub(  ",", ";", finalSetFilterSort$Consent)
-# 
-# write.table( finalSetFilterSort, file=paste0(file_path,"dbgapInfoUpdated.csv"), 
-#              sep = ',', 
-#              col.names = TRUE, 
-#              row.names = FALSE, 
-#              quote = FALSE)
-# 
-# 
 
 ##select the ones that are in TopMED freeze 5b
 topmed <- read.delim(paste0(file_path,"topmedFreeze5b.csv"), sep=',', colClasses = "character")
@@ -147,8 +99,6 @@ if( length(topmedphs[ topmedphs %in% finalSetFilterSort$dbGaP.TOPMed.Study.Acces
 if( length(topmedphs[! topmedphs %in% finalSetFilterSort$dbGaP.TOPMed.Study.Accession ]) != 0){
   print( paste0("The ones that are not present are: ", topmedphs[! topmedphs %in% finalSetFilterSort$dbGaP.TOPMed.Study.Accession ]))
 }
-
-
 
 finalData <- as.data.frame( matrix( ncol = ncol(finalSetFilterSort)))
 colnames(finalData) <- colnames(finalSetFilterSort)
@@ -185,9 +135,6 @@ for( i in 1:nrow(topmed)){
     }
 }
 
-
-
-
 finalSetFilterSort <- finalData[, c(1:17)]
 # Names with underscores
 colnames(finalSetFilterSort) <- c("Name","Country","Sample_Size","Subject_Count","Study_Design","Phenotypic_Variables",
@@ -201,104 +148,103 @@ finalSetFilterSort$Markerset <- gsub( ",", ";", finalSetFilterSort$Markerset)
 finalSetFilterSort$Ancestry <- gsub( ",", ";", finalSetFilterSort$Ancestry)
 finalSetFilterSort$Consent <- gsub(  ",", ";", finalSetFilterSort$Consent)
 
-
-
 # Function to hard-code set values 
 setValue <- function(BB_name, select_col, value){ 
   set(f5b, which(f5b[,Name==BB_name]), which(names(f5b)==select_col), value)
 }
 
-f5b = data.table(finalSetFilterSort) # freeze5bExtensive
+f5b = data.table(finalSetFilterSort) # freeze5b
 f5b = f5b[!is.na(Name),][!is.na(Phenotypic_Variables),]
-# Change columns to numeric
+# Change columns to integers
 f5b = f5b[,Sample_Size:=as.integer(Sample_Size)][,Subject_Count:=as.integer(Subject_Count)][,Phenotypic_Variables:=as.integer(Phenotypic_Variables)]
 setValue("GeneSTAR","Phenotypic_Variables", 157)
 # Filter to PhenoVars>=100 & Remove String values from PhenoVars 
 f5b = f5b[str_length(Phenotypic_Variables)<6,][Phenotypic_Variables>99,][order(-Phenotypic_Variables)]
-f5b = f5b[,Country:="USA"]# Registry cohort?? Maybe? [, Phenotypic_Data_Type:="TBD"] 
-f5b = f5b[, Accession:="Request access through dbGAP: https://dbgap.ncbi.nlm.nih.gov/aa/wga.cgi"]
-pubMed="https://www.ncbi.nlm.nih.gov/pubmed?cmd=DetailsSearch&term="
+f5b = f5b[,Country:="USA"][, Phenotypic_Data_Type:="Patient/Disease registries"] 
+f5b = f5b[, Accession:="<a href='https://dbgap.ncbi.nlm.nih.gov/aa/wga.cgi', target='_blank'>Request access through dbGAP</a>"]
+pubMed="<a href='https://www.ncbi.nlm.nih.gov/pubmed?cmd=DetailsSearch&term="
 
 # FHS - phs000974 (geno) ; phs000007 (pheno)
 # Ancestry: https://www.ncbi.nlm.nih.gov/projects/gap/population/cgi-bin/StudySubjectAncestry.cgi?phs=974&version=3&exp1=1&exp2=1&exp3=1
 setValue("FHS","Patients_Age", "28-62")
-setValue("FHS", "PubMedLink", paste0(pubMed,"5921755[PMID]"))
-setValue("FHS", "Phenotypic_Data_Type", "TBD")
+setValue("FHS", "PubMedLink", paste0(pubMed,"5921755[PMID]', target='_blank'>5921755</a>"))
+# setValue("FHS", "Phenotypic_Data_Type", "TBD")
 
 # MESA - phs001416 (geno) ; phs000209 (pheno)
 # Ancestry: https://www.ncbi.nlm.nih.gov/projects/gap/population/cgi-bin/StudySubjectAncestry.cgi?phs=1416&version=1&exp1=1&exp2=1&exp3=1
 setValue("MESA","Patients_Age", "45-84")
-setValue("MESA", "PubMedLink",  paste0(pubMed,"12397006[PMID]"))
-setValue("MESA", "Phenotypic_Data_Type", "TBD")
+setValue("MESA", "PubMedLink",  paste0(pubMed,"12397006[PMID]', target='_blank'>12397006</a>"))
+# setValue("MESA", "Phenotypic_Data_Type", "TBD")
 
 # ARIC - phs001211 (geno) ; phs000280 (pheno)
 # Ancestry: https://www.ncbi.nlm.nih.gov/projects/gap/population/cgi-bin/StudySubjectAncestry.cgi?phs=1211&version=2&exp1=1&exp2=1&exp3=1
 setValue("ARIC","Patients_Age", "45-64")
-setValue("ARIC", "PubMedLink",  paste0(pubMed,"2646917[PMID]"))
-setValue("ARIC", "Phenotypic_Data_Type", "TBD")
+setValue("ARIC", "PubMedLink",  paste0(pubMed,"2646917[PMID]', target='_blank'> </a>"))
+# setValue("ARIC", "Phenotypic_Data_Type", "TBD")
 
 # CHS - phs001368 (geno) ; phs000287 (pheno)
 # Ancestry: Not available for the initial cohort of 5,201 participants
 setValue("CHS","Patients_Age", ">65")
-setValue("CHS", "PubMedLink",  paste0(pubMed,"1669507[PMID]"))
-setValue("CHS", "Phenotypic_Data_Type", "TBD") # See Table 2 : https://reader.elsevier.com/reader/sd/pii/104727979190005W?token=52A6D06132598482C9EFD4EA215DBCAB80A92424DC3CD16B7DAB25823823D1BBE9C9F30FCA0FFB1BD78566ADE54DCF18
+setValue("CHS", "PubMedLink",  paste0(pubMed,"1669507[PMID]', target='_blank'> </a>"))
+# setValue("CHS", "Phenotypic_Data_Type", "TBD") # See Table 2 : https://reader.elsevier.com/reader/sd/pii/104727979190005W?token=52A6D06132598482C9EFD4EA215DBCAB80A92424DC3CD16B7DAB25823823D1BBE9C9F30FCA0FFB1BD78566ADE54DCF18
 
 # WHI - phs001237 (geno) ; phs000200 (pheno)
 # Ancestry: https://www.ncbi.nlm.nih.gov/projects/gap/population/cgi-bin/StudySubjectAncestry.cgi?phs=1237&version=1&exp1=1&exp2=1&exp3=1
 setValue("WHI","Patients_Age", "50-79") # https://www.whi.org/SitePages/WHI%20Home.aspx
 setValue("WHI", "PubMedLink", "https://doi.org/10.1089/jwh.1995.4.519" ) # No PubMed Link; https://www.whi.org/researchers/bibliography/SitePages/Advanced%20Search.aspx
-setValue("WHI", "Phenotypic_Data_Type", "TBD")
+# setValue("WHI", "Phenotypic_Data_Type", "TBD")
 
 # JHS - phs000964 (geno) ; phs000286 (pheno)
 setValue("JHS","Ancestry", "African American")
 setValue("JHS","Patients_Age", "35-84")
-setValue("JHS", "PubMedLink",  paste0(pubMed,"10100686[PMID]"))
-setValue("JHS", "Phenotypic_Data_Type", "TBD")
+setValue("JHS", "PubMedLink",  paste0(pubMed,"10100686[PMID]', target='_blank'> </a>"))
+# setValue("JHS", "Phenotypic_Data_Type", "TBD")
 
 # CFS - phs000954 (geno) ; phs000284 (pheno)
 # Ancestry: https://www.ncbi.nlm.nih.gov/projects/gap/population/cgi-bin/StudySubjectAncestry.cgi?phs=284&version=2&exp1=1&exp2=1&exp3=1
 setValue("CFS","Patients_Age", " 2-18")
-setValue("CFS", "PubMedLink",  paste0(pubMed,"10228121[PMID]"))
-setValue("CFS", "Phenotypic_Data_Type", "TBD")
+setValue("CFS", "PubMedLink",  paste0(pubMed,"10228121[PMID]', target='_blank'> </a>"))
+# setValue("CFS", "Phenotypic_Data_Type", "TBD")
 
 # GENOA - phs001345 (geno) ; phs001238 (pheno)
 # Ancestry: https://www.ncbi.nlm.nih.gov/projects/gap/population/cgi-bin/StudySubjectAncestry.cgi?phs=954&version=2&exp1=1&exp2=1&exp3=1
 setValue("GENOA","Patients_Age", "<60")
-setValue("GENOA", "PubMedLink",  paste0(pubMed,"15121494[PMID]"))
-setValue("GENOA", "Phenotypic_Data_Type", "TBD")
+setValue("GENOA", "PubMedLink",  paste0(pubMed,"15121494[PMID]', target='_blank'> </a>"))
+# setValue("GENOA", "Phenotypic_Data_Type", "TBD")
 
 # COPDGene - phs000951 (geno) ; phs000179 (pheno)
 # Ancestry: https://www.ncbi.nlm.nih.gov/projects/gap/population/cgi-bin/StudySubjectAncestry.cgi?phs=179&version=6&exp1=1&exp2=1&exp3=1
 setValue("COPDGene","Patients_Age", "45-80")
-setValue("COPDGene", "PubMedLink",  paste0(pubMed,"20214461[PMID]"))
-setValue("COPDGene", "Phenotypic_Data_Type", "TBD")
+setValue("COPDGene", "PubMedLink",  paste0(pubMed,"20214461[PMID]', target='_blank'> </a>"))
+# setValue("COPDGene", "Phenotypic_Data_Type", "TBD")
 
 # SAS - phs000972 (geno) ; phs000914 (pheno)
 setValue("SAS","Country", "Samoa")
 setValue("SAS","Ancestry", "Samoan")
 setValue("SAS","Patients_Age", "23-70")
-setValue("SAS", "PubMedLink",  paste0(pubMed,"24799123[PMID]"))
-setValue("SAS", "Phenotypic_Data_Type", "TBD")
+setValue("SAS", "PubMedLink",  paste0(pubMed,"24799123[PMID]', target='_blank'> </a>"))
+# setValue("SAS", "Phenotypic_Data_Type", "TBD")
 
 # HyperGEN - phs001293 (geno) 
 # Ancestry: https://www.ncbi.nlm.nih.gov/projects/gap/population/cgi-bin/StudySubjectAncestry.cgi?phs=954&version=2&exp1=1&exp2=1&exp3=1
 setValue("HyperGEN","Patients_Age", "<60")
-setValue("HyperGEN", "PubMedLink",  paste0(pubMed,"11115379[PMID]"))
-setValue("HyperGEN", "Phenotypic_Data_Type", "TBD")
+setValue("HyperGEN", "PubMedLink",  paste0(pubMed,"11115379[PMID]', target='_blank'> </a>"))
+# setValue("HyperGEN", "Phenotypic_Data_Type", "TBD")
 
 # GeneSTAR - phs001218 (geno) ; phs001074 (pheno)
 # Ancestry: https://www.ncbi.nlm.nih.gov/projects/gap/population/cgi-bin/StudySubjectAncestry.cgi?phs=1218&version=1&exp1=1&exp2=1&exp3=1
 setValue("GeneSTAR","Patients_Age", "<60")
-setValue("GeneSTAR", "PubMedLink",  paste0(pubMed,"16551714[PMID]"))
-setValue("GeneSTAR", "Phenotypic_Data_Type", "TBD")
+setValue("GeneSTAR", "PubMedLink",  paste0(pubMed,"16551714[PMID]', target='_blank'> </a>"))
+# setValue("GeneSTAR", "Phenotypic_Data_Type", "TBD")
 
 # GOLDN - phs001218 (geno) ; phs001074 (pheno)
 setValue("GOLDN","Ancestry", "Caucasian")
 setValue("GOLDN","Patients_Age", ">18")
-setValue("GOLDN", "PubMedLink",  paste0(pubMed,"22228203[PMID]"))
-setValue("GOLDN", "Phenotypic_Data_Type", "TBD")
+setValue("GOLDN", "PubMedLink",  paste0(pubMed,"22228203[PMID]', target='_blank'> </a>"))
+# setValue("GOLDN", "Phenotypic_Data_Type", "TBD")
 
-colnames(biobanks) <- c("Name","Country","Sample Size","Subject Count","Study Design","Phenotypic Variables",
+
+colnames(f5b) <- c("Name","Country","Sample Size","Subject Count","Study Design","Phenotypic Variables",
                         "Phenotypic Data Type","Molecular Data Type","Markerset","Disease/Focus",
                         "Patients Age (yrs)","Ancestry","Consent","Accession","Link","PubMed Link","Notes") 
 fwrite(f5b, file=paste0(file_path,"freeze5b.csv"), sep = ',', col.names = TRUE)
