c5b1ad48d944834648c1061d48144f512ebe58b8
aGutierrezSacristan <a.gutierrez.sacristan@gmail.com>
2019-10-11 16:33:00 -0400

    table generator script and shinyIo deploy script updated. biobanks new lines updated

--- shinyIo.R
+++ shinyIo.R
@@ -1,9 +1,9 @@
 install.packages('rsconnect')
 library( rsconnect )
-rsconnect::setAccountInfo(name='testestest',
-                          token='C1137C7EAE5DD22EF7F81A211B89FD58',
-                          secret='p7vU0syZOdGfA0oUKIodjkHP5BJ/XnbJ3YkiSk5M')
-rsconnect::deployApp('/Users/alba/Desktop/biobankCatalogShinyOnline/')
+rsconnect::setAccountInfo(name='avillachlab',
+                          token='1FAC8309DFDAAF16DD9E11B061A55BCA',
+                          secret='UKEqrmncJb8VFGXC57b8HeqxdFnyEY06/uRLl097')
+rsconnect::deployApp('/Users/alba/Desktop/geno-pheno-CatalogShinyOnline/', account = 'avillachlab')
 
 #if you remove the app from shinyIo then before deploying it again you have 
 #to remove the folder "rsconnect"
