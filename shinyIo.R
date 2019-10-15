e93e9ef45d1ced56fa90a7025e713a2c3d166f0e
aGutierrezSacristan <a.gutierrez.sacristan@gmail.com>
2019-10-15 17:10:38 -0400

    Delete shinyIo.R

--- shinyIo.R
+++
@@ -1,9 +0,0 @@
-install.packages('rsconnect')
-library( rsconnect )
-rsconnect::setAccountInfo(name='avillachlab',
-                          token='1FAC8309DFDAAF16DD9E11B061A55BCA',
-                          secret='UKEqrmncJb8VFGXC57b8HeqxdFnyEY06/uRLl097')
-rsconnect::deployApp('/Users/alba/Desktop/geno-pheno-CatalogShinyOnline/', account = 'avillachlab')
-
-#if you remove the app from shinyIo then before deploying it again you have 
-#to remove the folder "rsconnect"
