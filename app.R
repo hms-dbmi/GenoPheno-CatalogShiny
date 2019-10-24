2310ab28555d646e2e9365f5c07a3ae03e457f9f
aGutierrezSacristan <a.gutierrez.sacristan@gmail.com>
2019-10-24 14:16:29 -0400

    app updated and validation app updated

--- app.R
+++ app.R
@@ -405,7 +405,7 @@ for (var i = 0; i < tips.length; i++) {
     dataSubmission <- gsub(",", ";", dataSubmission)
     # Write the data to a temporary file locally
     filePath <- file.path(tempdir(), fileName)
-    write.csv(dataSubmission, filePath, quote = TRUE)
+    write.csv(dataSubmission, filePath, quote = TRUE, row.names = FALSE)
     # Upload the file to Dropbox
     drop_upload(filePath, path = outputDir)
     drop_upload(filePath, path = outputDirBckup)
